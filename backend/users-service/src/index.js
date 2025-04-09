65ddf17d177d66a0c3df15aa0c749a2015d0ca75
javicod92 <javi.javier.jc@gmail.com>
2025-04-09 15:19:18 -0300

    feat: add nodemon for live-reload in dev server

--- backend/users-service/src/index.js
+++ backend/users-service/src/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -12,8 +12,13 @@ app.use(cors());
 app.use(express.json());
 
 // Rutas
+app.get("/", async (req, res) => {
+  res.json("Probando desde la perla");
+});
+
 app.use("/api/auth", authRoutes);
 
+// Servidor
 app.listen(PORT, () => {
   console.log(`🟢 Users service escuchando en http://localhost:${PORT}`);
 });

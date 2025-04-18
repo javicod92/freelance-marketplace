cd896a79a1e46e5d00d9b73181e42eb367dfde70
javicod92 <javi.javier.jc@gmail.com>
2025-04-18 17:35:31 -0300

    feat: delete path of index.js in users-service

--- backend/users-service/src/index.js
+++ backend/users-service/src/index.js
@@ -1,13 +1,8 @@
 import express, { request, response } from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.join(__dirname, "../.env") });
+dotenv.config();
 
 const app = express();
 

990cc1d9a9cb97325b1a6a0cfab3b31764630be4
javicod92 <javi.javier.jc@gmail.com>
2025-04-22 00:01:59 -0300

    feat: add update, delete and obtain listing by id functions

--- backend/listings-service/src/controllers/listings.controller.js
+++ backend/listings-service/src/controllers/listings.controller.js
@@ -1,5 +1,6 @@
 import prisma from "../prisma/client.js";
 
+// Function to obtain all listings
 export const getAllListings = async (req, res) => {
   try {
     const listings = await prisma.listing.findMany();
@@ -10,6 +11,7 @@ export const getAllListings = async (req, res) => {
   }
 };
 
+// Function to create listing
 export async function createListing(req, res) {
   const { title, description, price } = req.body;
   const userId = req.user.id; // viene del token
@@ -31,3 +33,60 @@ export async function createListing(req, res) {
       .json({ error: "Error al crear listing", details: error.message });
   }
 }
+
+// Function to obtain the listing by id
+export const getListingById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const listing = await prisma.listing.findUnique({
+      where: { id },
+    });
+
+    if (!listing)
+      return res.status(404).json({ error: "Listing no encontrado" });
+
+    res.json(listing);
+  } catch (error) {
+    console.error("❌ Error al obtener el listing:", error);
+    res.status(500).json({ error: "Error al obtener el listing" });
+  }
+};
+
+// Function to udate the listing
+export const updateListing = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, price } = req.body;
+
+  try {
+    const listing = await prisma.listing.update({
+      where: { id },
+      data: {
+        title,
+        description,
+        price: Number(price),
+      },
+    });
+
+    res.json(listing);
+  } catch (error) {
+    console.error("❌ Error al actualizar el listing:", error);
+    res.status(500).json({ error: "Error al actualizar el listing" });
+  }
+};
+
+// Function to delete the listing by id
+export const deleteListing = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await prisma.listing.delete({
+      where: { id },
+    });
+
+    res.json({ message: "Listing eliminado correctamente" });
+  } catch (error) {
+    console.error("❌ Error al eliminar el listing:", error);
+    res.status(500).json({ error: "Error al eliminar el listing" });
+  }
+};
--- backend/listings-service/src/routes/listings.routes.js
+++ backend/listings-service/src/routes/listings.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import {
   createListing,
+  deleteListing,
   getAllListings,
+  getListingById,
+  updateListing,
 } from "../controllers/listings.controller.js";
 import { authenticateToken } from "../middlewares/auth.js";
 
@@ -9,5 +12,8 @@ const router = Router();
 
 router.get("/", getAllListings);
 router.post("/", authenticateToken, createListing);
+router.get("/:id", getListingById);
+router.put("/:id", updateListing);
+router.delete("/:id", deleteListing);
 
 export default router;

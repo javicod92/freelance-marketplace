e9ac7b3e3c50f8b9923b0df472c864862aec3d5e
javicod92 <javi.javier.jc@gmail.com>
2025-04-26 16:22:36 -0300

    adds route protection

--- backend/users-service/src/routes/auth.routes.js
+++ backend/users-service/src/routes/auth.routes.js
@@ -14,14 +14,24 @@ import { authorizeRole } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
+// Functions to manage user registration and login
 router.post("/register", validate(registerSchema), register); // Register a new user
 router.post("/login", validate(loginSchema), login); // Login an user
-router.get("/user/:id", getUserById); // Obtain user data throught an id
+
+// CRUD Functions to manage users data
+router.get("/profile/:id", authenticateToken, getUserById); // Obtain user data throught an id
 router.get("/profile", authenticateToken, (req, res) => {
   res.json({ user: req.user }); // Obtain the user data of the current user
 });
-router.put("/profile", authenticateToken, updateProfile); // Update data profile
+router.put(
+  "/profile",
+  authenticateToken,
+  validate(registerSchema),
+  updateProfile
+); // Update data profile
 router.delete("/profile", authenticateToken, deleteAccount); // Delete account
+
+// Admin functions. Allows administrators to get all users of the database
 router.get("/all", authenticateToken, authorizeRole("admin"), getAllUsers); // Obtain all users
 
 export default router;
